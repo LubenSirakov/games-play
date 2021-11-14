96b8940caa12c47953e173a4f333f63b17071743
Luben Sirakov <lubo95sirakov@gmail.com>
2021-11-14 21:35:06 +0200

    add simple page router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import { useState } from "react";
+
 import Header from "./components/Header.js";
 import WelcomeWorld from "./components/WelcomeWorld.js";
 import GameCatalog from "./components/GameCatalog.js";
 import CreateGame from "./components/CreateGame.js";
 
 function App() {
+  const [page, setPage] = useState('/home');
+
   const routes = {
-    '/home' : WelcomeWorld,
-    '/games': GameCatalog,
-    '/create-game': CreateGame
+    '/home': < WelcomeWorld />,
+    '/games': <GameCatalog />,
+    '/create-game': <CreateGame />
   };
+
+  const navigationChangeHandler = (path) => {
+    setPage(path);
+  }
 
   return (
     <div id="box">
 
-      <Header />
+      <Header navigationChangeHandler={navigationChangeHandler} />
 
       <main id="main-content">
-      <WelcomeWorld />
+        {routes[page] || <h2>No Page Found</h2>}
       </main>
 
     </div>
